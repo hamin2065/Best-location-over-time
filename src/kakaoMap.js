cd62b2af76295c626e67d74fe4935579b1e68bf6
구하민 <hamin2065@gmail.com>
2021-12-28 23:28:50 +0900

    add marker

--- src/kakaoMap.js
+++ src/kakaoMap.js
@@ -1,10 +1,19 @@
+import { markerdata } from "./markerData";
 const { kakao } = window;
 
 export default function kakaoMap() {
   const container = document.getElementById("myMap");
   const options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3,
+    center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
+    level: 5,
   };
   const map = new kakao.maps.Map(container, options);
+
+  markerdata.forEach((el) => {
+    new kakao.maps.Marker({
+      map: map,
+      position: new kakao.maps.LatLng(el.lat, el.lng),
+      title: el.title,
+    });
+  });
 }
